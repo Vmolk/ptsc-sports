ab8a51690eae4845ce29bc300d2ec33eeb40685c
Hoang Pham Viet <84380979+aliemaya@users.noreply.github.com>
2026-07-14 23:07:02 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
           />
           <div>
             <span className="brand-text">HỘI THAO</span>
-            <span className="brand-sub">Phòng Quản Lý Chất Lượng</span>
+            <span className="brand-sub">Phòng Quản Lý Chất CLượng</span>
           </div>
         </Link>
 
